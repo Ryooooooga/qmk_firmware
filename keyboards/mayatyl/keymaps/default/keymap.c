e2c81c7d82fe95f1e6b12740929c347533531bfa
Ryooooooga <eial5q265e5@gmail.com>
2025-09-26 17:47:16 +0900

    set keymatrix

--- keyboards/mayatyl/keymaps/default/keymap.c
+++ keyboards/mayatyl/keymaps/default/keymap.c
@@ -3,22 +3,28 @@
 
 #include QMK_KEYBOARD_H
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
+     * ┌───┬───┬───┬───┬───┬───┐
+     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Tab│ Q │ W │ E │ R │ T │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Cap│ A │ S │ D │ F │ G │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Sft│ Z │ X │ C │ V │ B │
+     * ├───┼───┼───┼───┼───┼───┤
+     * │Ctl│Win│Alt│   │
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_ESC,
+                                        KC_SPC, KC_BSPC, KC_DEL,
+                                            KC_HOME, KC_END
     )
 };
