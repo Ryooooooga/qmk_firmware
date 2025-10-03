ca702b74649f70d20d7f91ebb3f6a2e323ac4c8c
Ryooooooga <eial5q265e5@gmail.com>
2025-10-03 18:37:13 +0900

    update keymap

--- keyboards/mayatyl/keymaps/default/keymap.c
+++ keyboards/mayatyl/keymaps/default/keymap.c
@@ -24,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
         KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LCTL,  KC_LGUI, KC_LALT, KC_ESC,
-                                        KC_SPC, KC_BSPC, KC_DEL,
+        KC_LCTL,  KC_LGUI, KC_LALT, KC_LNG2,
+                                        KC_SPC, KC_TAB, KC_DEL,
                                             KC_HOME, KC_END,
 
         // Right
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,         KC_LBRC,  KC_RBRC,
         KC_H,    KC_J,    KC_K,     KC_L,    KC_SEMICOLON, KC_QUOTE, KC_BSLS,
         KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLASH,     KC_UP,
-                          KC_HOME,  KC_END,  KC_LEFT,      KC_DOWN,  KC_RIGHT,
-        KC_SPC, KC_ESC, KC_ENT,
-            KC_PGUP, KC_PGDN
+                          KC_LNG1,  KC_END,  KC_LEFT,      KC_DOWN,  KC_RIGHT,
+        KC_DEL, KC_ESC, KC_ENT,
+            KC_DEL, KC_BSPC
     )
 };
